5de6f39c7b2ed8a10f2f40fe99fabec37ebdb5fc
ritesh-negii <riteshnegi111@gmail.com>
2026-01-10 15:39:04 +0530

    chore: allow vercel frontend origin in CORS

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+    "https://expenseiq.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
